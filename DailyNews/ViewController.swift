fa501660df840be2be5bc518462a156c836934cc
burak cevizli <burak.cevizli7@gmail.com>
2023-03-08 16:50:49 +0300

    News TableView and TableViewCell

--- DailyNews/ViewController.swift
+++ DailyNews/ViewController.swift
@@ -15,14 +15,58 @@ import UIKit
 
 // dfe7e9395d094c758864fe0ec6ca9e4b
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        return table
+    }()
+    
+    private var viewModels = [NewsTableViewCellViewModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "News"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .systemBackground
+        
+        APICaller.shared.getTopStories { [weak self] result in
+            switch result {
+            case .success(let articles):
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(title: $0.title, subtitle: $0.description ?? "No Description", imageURL: URL(string: $0.urlToImage ?? ""))
+                    
+                })
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    // Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as? NewsTableViewCell else {
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 
